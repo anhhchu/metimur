09c687218a1d5676744e72b303674e7a775aa324
anhhchu <anhhchu12@gmail.com>
2024-03-06 00:11:35 -0800

    Update tpch query

--- queries/tpch/1.sql
+++ queries/tpch/1.sql
@@ -147,8 +147,8 @@ from
 where
   l_shipdate >= date('1993-01-01')
   and l_shipdate < date('1994-01-01')
-  and l_discount between.06 - 0.01
-  and.06 + 0.01
+  and l_discount between .06 - 0.01
+  and .06 + 0.01
   and l_quantity < 24;
 
 Q07
